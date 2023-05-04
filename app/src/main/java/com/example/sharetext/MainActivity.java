3000c4380fa2df1922eaf3dec15cbf20bbfb6866
keymarrow <pkimaro11@gmail.com>
2023-05-04 10:04:29 +0300

    initial commit

---
+++ app/src/main/java/com/example/sharetext/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.sharetext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private Button shareButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        shareButton = findViewById(R.id.shareText);
+
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = editText.getText().toString();
+
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.setType("text/plain");
+                shareIntent.putExtra(Intent.EXTRA_TEXT, text);
+                startActivity(Intent.createChooser(shareIntent, "Share via"));
+            }
+        });
+    }
+}
